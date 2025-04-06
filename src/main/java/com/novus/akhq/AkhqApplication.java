275866a26beb68ecd5b5aa0104883e3ca90b26ce
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-06 15:01:03 +0200

    First commit

---
+++ src/main/java/com/novus/akhq/AkhqApplication.java
@@ -0,0 +1,13 @@
+package com.novus.akhq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkhqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkhqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novus/akhq/AkhqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novus.akhq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkhqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
